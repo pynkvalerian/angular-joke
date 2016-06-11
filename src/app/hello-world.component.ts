37011d37457af5cc9668bbf41578cce474bde0fe
pynkvalerian <yenping.chew90@gmail.com>
2016-06-11 12:09:40 +0800

    hello world done

---
+++ src/app/hello-world.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hello-world-app',
+  templateUrl: 'hello-world.component.html',
+  styleUrls: ['hello-world.component.css']
+})
+
+export class HelloWorldAppComponent {
+  title = 'hello-world!';
+
+  yourName: string = '';
+
+  sayHello(name: string) {
+    window.alert('Hello ' + name + '!');
+  }
+}
